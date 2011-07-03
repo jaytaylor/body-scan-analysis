50739489a1f6ec54ada7cb54810cc90909b96634
Jay Taylor <outtatime@gmail.com>
2011-07-03 13:22:34 -0700

    Incrementing state onClick works

--- bsa.js
+++ bsa.js
@@ -8,6 +8,78 @@ if ('undefined' === typeof console) {
     this.console = { log: function() {} };
 }
 
+var data = {
+    "gridMap": {
+        "xAxis": [
+            0,
+            37,
+            80,
+            124,
+            157
+        ],
+        "yAxis": [
+            0,
+            27,
+            48,
+            69,
+            110,
+            130,
+            150,
+            170,
+            188,
+            208,
+            217,
+            227,
+            248,
+            266,
+            286,
+            308,
+            331,
+            360,
+            384,
+            413,
+            442
+        ]
+    },
+    "disabledTileIds": [
+        0,
+        1,
+        2,
+        3,
+        14,
+        15,
+        16,
+        17,
+        18,
+        63,
+        64,
+        65,
+        66,
+        77,
+        78,
+        79,
+        80,
+        81
+    ],
+    "mergeTileIds": [
+        [4, 5],
+        [8, 9],
+        [10, 11],
+        [12, 13],
+        [19, 40],
+        [30, 31],
+        [51, 52],
+        [61, 82],
+        [64, 65],
+        [69, 70],
+        [71, 72],
+        [73, 74],
+        [75, 76],
+        [58, 78]
+    ],
+};
+
+
 var x; //drawing context
 var width;
 var height;
@@ -16,162 +88,205 @@ var buffer;
 
 $(document).ready(function() {
 
+	var BSAImage = Class.extend({
+		init: function(srcImages, data) {
+			this.images = [];
+            _.each(srcImages, function(src) {
+                var sz = this.images.push(new Image());
+			    this.images[--sz].src = src;
+            }, this);
+
+            this.bgImage = this.images[0];
+
+			this.data = data;
+
+            this.state = {};
+            this.boxAtTileId = {};
+
+            // Calculate the maximum possible tile id.
+            var tileId = this.coordsToTileId(
+                this.data.gridMap.xAxis[this.data.gridMap.xAxis.length - 1] - 1,
+                this.data.gridMap.yAxis[this.data.gridMap.yAxis.length - 1] - 1
+            );
+            while (tileId >= 0) {
+                var box = this._tileIdToBox(tileId)
+                if (null !== box) {
+                    this.boxAtTileId[tileId] = box;
+                }
+                this.state[tileId--] = 0;
+            }
+
+            // Fill a merge object for easy use.
+            this.data.mergeInfo = {};
+            _.each(data.mergeTileIds, function(list) {
+                _.each(list, function(tileId) {
+                    this.data.mergeInfo[tileId] = list;
+                }, this);
+            }, this);
+
+            return this;
+		},
+
+        coordsToTileId: function(x, y) {
+            var xAxis = this.data.gridMap.xAxis,
+                yAxis = this.data.gridMap.yAxis;
+            // Special case for tile 0.
+            if (x > xAxis[0] && x <= xAxis[1] && y > yAxis[0] && y <= xAxis[1]) {
+                // tileId = 0.
+                $('#coordinates').html(x + ', ' + y + ' px' + ', #0');
+                if (-1 !== this.data.disabledTileIds.indexOf(0)) {
+                    return null;
+                } else {
+                    return 0;
+                }
+            }
+            for (var xLine = xAxis[1], i = 1; i < xAxis.length; xLine = xAxis[++i]) {
+                for (var yLine = yAxis[1], j = 1; j < yAxis.length; yLine = yAxis[++j]) {
+                    var tileId = (xAxis.indexOf(xLine) - 1) * yAxis.length + (yAxis.indexOf(yLine) - 1);
+                    if (x < xLine && x >= xAxis[xAxis.indexOf(xLine) - 1] &&
+                        y < yLine && y >= yAxis[yAxis.indexOf(yLine) - 1]) {
+                        // Don't want disabled tiles.
+                        $('#coordinates').html(x + ', ' + y + ' px' + ', #' + tileId);
+                        if (-1 !== this.data.disabledTileIds.indexOf(tileId)) {
+                            console.log("found disabled tile: " + tileId);
+                            tileId = null;
+                        }
+                        return tileId;
+                    }
+                }
+            }
+            return null;
+        },
+
+        /**
+         * This is used for pre-computing.  Should not be invoked externally.
+         * @todo hide this so it's not externall accessible.
+         */
+        _tileIdToBox: function(tileId) {
+            var xAxis = this.data.gridMap.xAxis,
+                yAxis = this.data.gridMap.yAxis;
+            // Special case for tile 0.
+            if (0 === tileId) {
+                var box = {
+                    topLeft: {
+                        x: xAxis[0],
+                        y: yAxis[0]
+                    },
+                    bottomRight: {
+                        x: xAxis[1],
+                        y: yAxis[1]
+                    },
+                };
+                return box;
+            }
+            for (var xLine=xAxis[1], i=1; i < xAxis.length; xLine=xAxis[++i]) {
+                for (var yLine=yAxis[1], j=1; j < yAxis.length; yLine=yAxis[++j]) {
+                    var tid = (xAxis.indexOf(xLine) - 1) * yAxis.length + (yAxis.indexOf(yLine) - 1);
+                    if (tid === tileId) {
+                        var box = {
+                            topLeft: {
+                                x: xAxis[xAxis.indexOf(xLine) - 1],
+                                y: yAxis[yAxis.indexOf(yLine) - 1]
+                            },
+                            bottomRight: {
+                                x: xLine,
+                                y: yLine
+                            },
+                        };
+                        return box;
+                    }
+                }
+            }
+            return null;
+        },
+
+        fillTile: function(tileId, destImage, context) {
+            var tileIds = [tileId];
+            if ('undefined' !== typeof this.data.mergeInfo[tileId]) {
+                tileIds = this.data.mergeInfo[tileId];
+            }
+            _.each(tileIds, function(tileId) {
+                console.log(tileId);
+                console.log(context);
+                var box = this.boxAtTileId[tileId];
+                context.drawImage(
+                    destImage,
+                    box.topLeft.x,
+                    box.topLeft.y,
+                    box.bottomRight.x - box.topLeft.x,
+                    box.bottomRight.y - box.topLeft.y,
+                    box.topLeft.x,
+                    box.topLeft.y,
+                    box.bottomRight.x - box.topLeft.x,
+                    box.bottomRight.y - box.topLeft.y
+                );
+            }, this);
+        },
+
+        clearTile: function(tileId, context) {
+            var tileIds = [tileId];
+            if ('undefined' !== typeof this.data.mergeInfo[tileId]) {
+                tileIds = this.data.mergeInfo[tileId];
+            }
+            _.each(tileIds, function(tileId) {
+                var box = this.boxAtTileId[tileId];
+                context.clearRect(
+                    box.topLeft.x,
+                    box.topLeft.y,
+                    box.bottomRight.x - box.topLeft.x,
+                    box.bottomRight.y - box.topLeft.y
+                );
+            }, this);
+        },
+
+        tileClick: function(e) {
+            var box = CanvasMouse.getBox();
+            var tileId = CanvasMouse.getTileId();
+            //console.log(box);
+            console.log(this);
+            if (null != tileId && null !== box) {
+                this.state[tileId] = ++this.state[tileId] % 4;
+                if (0 === this.state[tileId]) {
+                    img.clearTile(tileId, fgContext);
+                } else {
+                    img.fillTile(tileId, this.images[this.state[tileId]], fgContext);
+                }
+            }
+        }
+
+	});
+
     var bgCanvas = $('.bg')[0];//document.getElementById('canvasLayer0');
     var bgContext = bgCanvas.getContext('2d');
 
     var fgCanvas = $('.fg')[0];//document.getElementById('canvasLayer1');
     var fgContext = fgCanvas.getContext('2d');
 
-	var img = new Image();
-	img.src = "childFront.png";
+    var imageLocations = [
+        "childFront.png",
+        "childFront-1.png",
+        "childFront-2.png",
+        "childFront-3.png",
+        "childFront-4.png"
+    ];
+	var img = new BSAImage(imageLocations, data);
 
-    img.offset = {top: 0, left: 0};
+    _.each(img.images, function(image) {
+        image.offset = {top: 0, left: 0};
+    });
 
-    img.onload = function() {
-        bgContext.drawImage(img, img.offset.top, img.offset.left, img.width, img.height);
-		Pixastic.process(img, "coloradjust", {red:0.5,green:0,blue:0});
+    img.bgImage.onload = function() {
+        bgContext.drawImage(img.bgImage, img.bgImage.offset.top, img.bgImage.offset.left, img.bgImage.width, img.bgImage.height);
     }
 
-    img.data = {
-        "gridMap": {
-            "xAxis": [
-                0,
-                38,
-                80,
-                124,
-                157
-            ],
-            "yAxis": [
-                0,
-                27,
-                48,
-                69,
-                130,
-                150,
-                170,
-                188,
-                208,
-                227,
-                248,
-                266,
-                286,
-                308,
-                331,
-                360,
-                384,
-                413,
-                442
-            ]
-        },
-        "disabledTileIds": [
-            0,
-            1,
-            2,
-            12,
-            13,
-            14,
-            15,
-            16,
-            57,
-            58,
-            59,
-            69,
-            70,
-            71,
-            72,
-            73
-        ],
-        "mergeTileIds": [
-            [3, 4],
-            [17, 36],
-            [60, 61],
-            [55, 74]
-        ],
-    };
-
-    img.coordsToTileId = function(x, y) {
-        var foundTile = null,
-            xAxis = this.data.gridMap.xAxis,
-            yAxis = this.data.gridMap.yAxis;
-        // Special case for tile 0.
-        if (x > xAxis[0] && x <= xAxis[1] && y > yAxis[0] && y <= xAxis[1]) {
-            return 0;
-        }
-        for (var xLine=xAxis[1], i=1; null === foundTile && i < xAxis.length; xLine=xAxis[++i]) {
-            for (var yLine=yAxis[1], j=1; null === foundTile && j < yAxis.length; yLine=yAxis[++j]) {
-                var tileId = (xAxis.indexOf(xLine) - 1) * yAxis.length + (yAxis.indexOf(yLine) - 1);
-                if (null === foundTile &&
-                    x < xLine && x >= xAxis[xAxis.indexOf(xLine) - 1] &&
-                    y < yLine && y >= yAxis[yAxis.indexOf(yLine) - 1]) {
-                    // Don't want disabled tiles.
-                    if (-1 !== img.data.disabledTileIds.indexOf(tileId)) {
-                        tileId = null;
-                    }
-                    return tileId;
-                }
-            }
-        }
-        return null;
-    };
-
-    img.tileIdToBox = function(tileId) {
-        var xAxis = this.data.gridMap.xAxis,
-            yAxis = this.data.gridMap.yAxis;
-        // Special case for tile 0.
-        if (0 === tileId) {
-            var box = {
-                topLeft: {
-                    x: xAxis[0],
-                    y: yAxis[0]
-                },
-                bottomRight: {
-                    x: xAxis[1],
-                    y: yAxis[1]
-                },
-            };
-            return box;
-        }
-        for (var xLine=xAxis[1], i=1; i < xAxis.length; xLine=xAxis[++i]) {
-            for (var yLine=yAxis[1], j=1; j < yAxis.length; yLine=yAxis[++j]) {
-                var tid = (xAxis.indexOf(xLine) - 1) * yAxis.length + (yAxis.indexOf(yLine) - 1);
-                if (tid === tileId) {
-                    var box = {
-                        topLeft: {
-                            x: xAxis[xAxis.indexOf(xLine) - 1],
-                            y: yAxis[yAxis.indexOf(yLine) - 1]
-                        },
-                        bottomRight: {
-                            x: xLine,
-                            y: yLine
-                        },
-                    };
-                    return box;
-                }
-            }
-        }
-        return null;
-    };
-
-    img.state = (function(img) {
-        var state = {};
-        var maxTileId = img.coordsToTileId(
-            img.data.gridMap.xAxis[img.data.gridMap.xAxis.length - 1] - 1,
-            img.data.gridMap.yAxis[img.data.gridMap.yAxis.length - 1] - 1
-        );
-        while (maxTileId >= 0) {
-            state[maxTileId--] = 0;
-        }
-        return state;
-    })(img);
-
     _.each(img.data.gridMap.xAxis, function(x) {
-        fgContext.moveTo(x + img.offset.left, 0 + img.offset.top);
-        fgContext.lineTo(x + img.offset.left, img.height + img.offset.top);
+        fgContext.moveTo(x + img.bgImage.offset.left, 0 + img.bgImage.offset.top);
+        fgContext.lineTo(x + img.bgImage.offset.left, img.bgImage.height + img.bgImage.offset.top);
     });
 
     _.each(img.data.gridMap.yAxis, function(y) {
-        fgContext.moveTo(0 + img.offset.left, y + img.offset.top);
-        fgContext.lineTo(img.width + img.offset.left, y + img.offset.top);
+        fgContext.moveTo(0 + img.bgImage.offset.left, y + img.bgImage.offset.top);
+        fgContext.lineTo(img.bgImage.width + img.bgImage.offset.left, y + img.bgImage.offset.top);
     });
 
     fgContext.strokeStyle = '#99ffff';
@@ -192,17 +307,17 @@ $(document).ready(function() {
         $('canvas').mousemove(function(e) {
             _x = e.pageX - _canvasOffset.left;
             _y = e.pageY - _canvasOffset.top;
-            //_box = null;
             var tileId = img.coordsToTileId(_x, _y);
-            if (null !== tileId && _lastTileId !== tileId) {
-                //_canvas.width = _canvas.width; // Reset canvas contents.
-                _box = img.tileIdToBox(tileId);
+            if (null !== tileId && _lastTileId !== tileId &&
+                'undefined' !== typeof img.boxAtTileId[tileId]) {
+                _box = img.boxAtTileId[tileId];
             }
+			//console.log("setting ltid=" + tileId);
             _lastTileId = tileId;
 
             var tileStr = null !== _lastTileId ? ', tileId=' + _lastTileId : '';
-            $('#coordinates').html(_x + ', ' + _y + ' px' + tileStr);
         });
+        _canvas.addEventListener('click', function(e) {img.tileClick(e);}, false);
 
         return {
             canvas: function() {
@@ -212,6 +327,7 @@ $(document).ready(function() {
                 return _box;
             },
             getTileId: function() {
+				//console.log('gettileId:' + _lastTileId);
                 return _lastTileId;
             },
             getLastCoordinates: function() {
@@ -219,35 +335,6 @@ $(document).ready(function() {
             }
         };
     })(img);
-
-    CanvasMouse.canvas().addEventListener('click', tileClick, false);
-
-    function tileClick(e) {
-        var box = CanvasMouse.getBox();
-        var tileId = CanvasMouse.getTileId();
-        console.log(box);
-        if (null !== box) {
-            if (0 === img.state[tileId]) {
-                img.state[tileId] = 1;
-                fgContext.fillRect
-            (
-                box.topLeft.x,
-                box.topLeft.y,
-                box.bottomRight.x - box.topLeft.x,
-                box.bottomRight.y - box.topLeft.y
-            );
-            } else {
-                img.state[tileId] = 0;
-                fgContext.clearRect
-            (
-                box.topLeft.x,
-                box.topLeft.y,
-                box.bottomRight.x - box.topLeft.x,
-                box.bottomRight.y - box.topLeft.y
-            );
-            }
-        }
-    }
 
     console.log('done');
 });
